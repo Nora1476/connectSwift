b7a730acdeff511b7c9fdeaae746f36a7bd05b89
Bonghui(Nora) <nada70979@gmail.com>
2024-05-20 10:19:45 +0900

    Initial Commit

---
+++ connectSwift/connectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectSwiftApp.swift
+//  connectSwift
+//
+//  Created by BonghuiJo on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct connectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
