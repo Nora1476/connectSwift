48862f10bf4ebdadde686cd1521e3fa84a73db28
Bonghui(Nora) <nada70979@gmail.com>
2024-05-23 18:33:21 +0900

    240523

--- connectSwift/ConnectSDK/WebOSTVService.swift
+++ connectSwift/ConnectSDK/WebOSTVService.swift
@@ -9,23 +9,16 @@ import Foundation
 import ConnectSDK
 
 
-class WebOSTVService:NSObject, ConnectableDeviceDelegate{
+class WebOSTVService:NSObject{
     private var device: ConnectableDevice?
     private var webOSService: WebOSTVService?
     
     func initialize(device: ConnectableDevice){
-
+        
     }
     
-    func connectableDeviceReady(_ device: ConnectableDevice!) {
-        print("Device is ready")
-        <#code#>
-    }
     
-    func connectableDeviceDisconnected(_ device: ConnectableDevice!, withError error: (any Error)!) {
-        print("Device disconnected: \(String(describing: error))")
-        <#code#>
-    }
 }
+
 
 
